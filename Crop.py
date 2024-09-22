Fixed CSV parser (final change) so that time values are in correct units. Added function to crop a set of images with the same timing.

Crop.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 # ---------- File Paths ----------
 
 source = '../../Interferometry Data (Raw)/8_23_2024' # Directory containing raw images
-target = 'Cropped Data' # Target directory to deposit cropped images
+target = '../Cropped Data' # Target directory to deposit cropped images
 shot_info = target + '/Shot Info.txt' # Text file containing timing and assoicated shot numbers
 
 # ---------- Parameters ----------
@@ -27,10 +27,11 @@ LY = 2871
 # ---------- Functions ----------
 
 def getShotInfo(info_path=shot_info): # Get shot information from Shot Info text file
-    info = pd.read_csv(info_path,delimiter=',',engine='python',converters={'Time (ms)': str,'Initial Shot': str,'Final Shot': str}).to_numpy()
+    #info = pd.read_csv(info_path,delimiter=',',engine='python',converters={'Time (ms)': str,'Initial Shot': str,'Final Shot': str}).to_numpy() # Read Shot Info text file, preserving leading zeros
+    info = pd.read_csv(info_path,delimiter=',',engine='python').to_numpy() # Read Shot Info text file
     time = (info[:,0].astype(float))*1e6 # Get shot time in ns
     time = abs(time - time[0]) # Set initial time to zero
-    Ni,Nf = info[:,1],info[:,2] # Initial and final shot number for each time setting
+    Ni,Nf = info[:,1].astype(int),info[:,2].astype(int) # Initial and final shot number for each time setting
     print(f'{time}\n{Ni}\n{Nf}')
     numSets = len(time) # Number of datasets taken
     return time,Ni,Nf,numSets
@@ -42,10 +43,12 @@ def cropSingle(shotNum,LX=LX,LY=LY,UX=UX,UY=UY,source_path=source,targ_path=targ
     plt.savefig(f'{targ_path}/{shotNum}_interferometer.jpg')
 
 def cropSet(Ni,Nf): # Crop set of images for one time setting
-    return 0
-
+    numShots = Nf - Ni + 1 # Determine number of shots in the set
+    for i in np.linspace(0,numShots-1,numShots,dtype='int'): # Loop through, crop each image
+        cropSingle(str(Ni + i).zfill(5)) # zfill pads the shot number with leading zeros
 
 #shot = '00247'
 #cropSingle(shot)
 
-getShotInfo()
+#getShotInfo()
+cropSet(int(21),int(29))