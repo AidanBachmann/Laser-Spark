ebd0d1f6e6fdbf96e8c8313477fe38cbfa5333b1
Aidan <abachma3@u.rochester.edu>
2024-09-21 21:13:12 -0700

    Wrote basic cropping function, tested on sample shot

--- Crop.py
+++ Crop.py
@@ -6,14 +6,32 @@
 # ---------- Imports ----------
 
 import numpy as np
-import PIL
+from PIL import Image
 import os
 import matplotlib.pyplot as plt
 
 # ---------- File Paths ----------
 
-source = '../Interferometry Data (Raw)/8_23_2024/' # Directory containing raw images
-target = 'Cropped Data/' # Target directory to deposit cropped images
+source = '../../Interferometry Data (Raw)/8_23_2024' # Directory containing raw images
+target = 'Cropped Data' # Target directory to deposit cropped images
 shot_info = target + 'Shot Info.txt' # Text file containing timing and assoicated shot numbers
 
+# ---------- Parameters ----------
+
+UX = 2793 # x and y pixels for upper left corner of crop
+UY = 1935
+
+LX = 4525 # x and y pixels for lower right corner of crop
+LY = 2871
+
 # ---------- Functions ----------
+
+def cropSingle(shotNum,LX=LX,LY=LY,UX=UX,UY=UY,source_path=source,targ_path=target):
+    arr = np.asarray(Image.open(f'{source_path}/{shotNum}_interferometer.jpg').convert('L')) # Open image in greyscale, store in array
+    croppedArr = arr[UY:LY+1,UX:LX+1] # Crop Image
+    plt.imshow(croppedArr)
+    plt.savefig(f'{targ_path}/{shotNum}_interferometer.jpg')
+
+
+shot = '00247'
+cropSingle(shot)
