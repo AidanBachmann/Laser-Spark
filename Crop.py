9c5956029e23647e2a9da58813511ca4998dc009
Aidan <abachma3@u.rochester.edu>
2024-09-21 21:25:16 -0700

    Added CSV parser for getting shot timing and associated shot numbers from the "Shot Info.txt" file.

--- Crop.py
+++ Crop.py
@@ -7,14 +7,14 @@
 
 import numpy as np
 from PIL import Image
-import os
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # ---------- File Paths ----------
 
 source = '../../Interferometry Data (Raw)/8_23_2024' # Directory containing raw images
 target = 'Cropped Data' # Target directory to deposit cropped images
-shot_info = target + 'Shot Info.txt' # Text file containing timing and assoicated shot numbers
+shot_info = target + '/Shot Info.txt' # Text file containing timing and assoicated shot numbers
 
 # ---------- Parameters ----------
 
@@ -26,12 +26,21 @@ LY = 2871
 
 # ---------- Functions ----------
 
-def cropSingle(shotNum,LX=LX,LY=LY,UX=UX,UY=UY,source_path=source,targ_path=target):
+def getShotInfo(info_path=shot_info): # Get shot information from Shot Info text file
+    info = pd.read_csv(info_path,delimiter=',',engine='python').to_numpy()
+    time = abs((info[:,0] - info[0,0])*(1e3)) # Get shot time in ns
+    Ni,Nf = info[:,1].astype(int),info[:,2].astype(int) # Initial and final shot number for each time setting
+    print(time,'\n',Ni,'\n',Nf)
+    return time,Ni,Nf
+
+def cropSingle(shotNum,LX=LX,LY=LY,UX=UX,UY=UY,source_path=source,targ_path=target): # Crop single image, save in target directory
     arr = np.asarray(Image.open(f'{source_path}/{shotNum}_interferometer.jpg').convert('L')) # Open image in greyscale, store in array
     croppedArr = arr[UY:LY+1,UX:LX+1] # Crop Image
     plt.imshow(croppedArr)
     plt.savefig(f'{targ_path}/{shotNum}_interferometer.jpg')
 
 
-shot = '00247'
-cropSingle(shot)
+#shot = '00247'
+#cropSingle(shot)
+
+getShotInfo()
