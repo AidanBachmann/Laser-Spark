0fc1f1913608940ef5d204f5f53abe7ebd9d73cd
Aidan <abachma3@u.rochester.edu>
2024-09-24 12:04:46 -0700

    Added shockVelocity.py for computing shock speed.

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # ---------- Imports ----------
 
 import time as TIME
-import Crop as crop
+import Crop as CROP
 import signalProcessing as SP
+import shockVelocity as SV
 
 # ---------- Flags ----------
 
-to_do = 'pass' # Sets function of main. Options are 'crop' and 'unwrap'.
+to_do = 'pass' # Sets function of main. Options are 'crop', 'unwrap', and 'findv'.
 
 # ---------- Main ----------
 
 start = TIME.time()
 if to_do == 'crop':
-    crop.cropAll() # Crop all images
+    CROP.cropAll() # Crop all images
 elif to_do == 'unwrap':
     #SP.unwrapSingle(shotNumber=21) # 165
     SP.unwrapAll()
+elif to_do == 'findv':
+    pass
 else:
     print('Unknown function. Options are "crop" and "unwrap".')
 end = TIME.time()
